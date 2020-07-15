4bbbd1561e04baf38e544956a3ad204cd933d6e0
Oskar <case112@users.noreply.github.com>
2020-07-15 18:23:57 +0300

    Create read-voltage.py

---
+++ read-voltage.py
@@ -0,0 +1,19 @@
+import serial
+import time
+import sys
+
+
+def voltage_state():
+
+    counter = 0
+    data_list = []
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser.flush()
+
+    while counter < 3:
+        if ser.in_waiting > 0:
+            voltage = ser.readline().decode('utf-8').rstrip()
+            counter += 1
+            time.sleep(2.0)
+
+    return voltage
